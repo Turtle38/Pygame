64d62ad894c1b12dd665b81e165ecf3d18f1e8be
Daphné Di Malta <daphne38250@gmail.com>
2021-05-04 20:24:02 +0200

    fenêtreResteOuverte

--- Test.py
+++ Test.py
@@ -4,5 +4,13 @@ pygame.init()
 
 ######Générer la fenêtre du jeu"""#######
 pygame.display.set_caption("Comet fall game")
-pygame.display.set_mode((500,300))
+pygame.display.set_mode((1080,720))
 
+running = True
+
+#####boucle tant que cette condition est vrai ###
+while running:
+    for event in pygame.event.get():
+        if event.type ==pygame.QUIT:
+            running = False
+            pygame.quit()
