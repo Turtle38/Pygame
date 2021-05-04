6e50ed12395e4c6320f2e62a196f1168629e1d5a
Daphné Di Malta <daphne38250@gmail.com>
2021-05-04 23:26:29 +0200

    class jeau et déplacement des classes

--- Test.py
+++ Test.py
@@ -1,18 +1,11 @@
 import pygame
+from game import Game
+from player import Player
 pygame.init()
 
-##creation de la class joueur
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.health = 100
-        self.max_health = 100
-        self.attack = 10
-        self.velocity = 5
-        self.image = pygame.image.load("assets/player.png")
-        self.rect = self.image.get_rect()
-        self.rect.x = 400
-        self.rect.y = 500
+
+
+
 
 ######Générer la fenêtre du jeu"""#######
 pygame.display.set_caption("Comet fall game")
@@ -20,8 +13,9 @@ screen = pygame.display.set_mode((1080,720))
 ###importer background######
 background = pygame.image.load("assets/bg.jpg ")
 
-####charger notre joueur###
-player = Player()
+###Charger le jeu
+game = Game()
+
 
 
 running = True
@@ -32,7 +26,7 @@ while running:
     screen.blit(background,(0,-200))
 
     ###appliquer l'image du joueur###
-    screen.blit(player.image,player.rect)
+    screen.blit(game.player.image,game.player.rect)
 
     ##mettre à jour l'écran 
     pygame.display.flip()
---
+++ game.py
@@ -0,0 +1,7 @@
+import pygame
+from player import Player
+###création d'une classe jeau 
+class Game:
+    def __init__(self):
+        ###générer le joueur
+        self.player = Player()
