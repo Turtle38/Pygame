3e3c7b96444a6fc03f5ec74dc9fff55de0bcfe1e
Daphné Di Malta <daphne38250@gmail.com>
2021-05-04 20:39:51 +0200

    Background

--- Test.py
+++ Test.py
@@ -4,13 +4,20 @@ pygame.init()
 
 ######Générer la fenêtre du jeu"""#######
 pygame.display.set_caption("Comet fall game")
-pygame.display.set_mode((1080,720))
+screen = pygame.display.set_mode((1080,720))
+
+background = pygame.image.load("assets/bg.jpg ")
 
 running = True
 
 #####boucle tant que cette condition est vrai ###
 while running:
+    ###appliquer l'arrière plan du jeu###
+    screen.blit(background,(0,-200))
+    ##mettre à jour l'écran 
+    pygame.display.flip()
     for event in pygame.event.get():
         if event.type ==pygame.QUIT:
             running = False
             pygame.quit()
+
