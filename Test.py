e1af57045c5ca81c2cc766b86b8b084e06809283
Daphné Di Malta <daphne38250@gmail.com>
2021-05-04 23:10:30 +0200

    ajout du Joueur

--- Test.py
+++ Test.py
@@ -1,12 +1,28 @@
 import pygame
 pygame.init()
 
+##creation de la class joueur
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.health = 100
+        self.max_health = 100
+        self.attack = 10
+        self.velocity = 5
+        self.image = pygame.image.load("assets/player.png")
+        self.rect = self.image.get_rect()
+        self.rect.x = 400
+        self.rect.y = 500
 
 ######Générer la fenêtre du jeu"""#######
 pygame.display.set_caption("Comet fall game")
 screen = pygame.display.set_mode((1080,720))
-
+###importer background######
 background = pygame.image.load("assets/bg.jpg ")
+
+####charger notre joueur###
+player = Player()
+
 
 running = True
 
@@ -14,6 +30,10 @@ running = True
 while running:
     ###appliquer l'arrière plan du jeu###
     screen.blit(background,(0,-200))
+
+    ###appliquer l'image du joueur###
+    screen.blit(player.image,player.rect)
+
     ##mettre à jour l'écran 
     pygame.display.flip()
     for event in pygame.event.get():
